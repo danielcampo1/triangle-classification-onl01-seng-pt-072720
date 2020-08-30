9a67ba303313ce119194962d78d3da5dfe2e4a4d
daniel campo <campo_daniel94@yahoo.com>
2020-08-30 16:32:42 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,14 @@
 class Triangle
-  # write code here
+  
+  attr_accessor :quilateral, :isosceles, :scalene
+  
+  def initialize(quilateral, isosceles, scalene)
+    @quilateral = quilateral
+    @isosceles = isosceles
+    @scalene = scalene
+  end
+  
+  def kind(symbol,type,valid_types)
+  end
+  
 end
