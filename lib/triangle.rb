32cbad8964ca39de3158a9366c53c0ee4f929d95
daniel campo <campo_daniel94@yahoo.com>
2020-08-30 16:39:05 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,14 +1,38 @@
 class Triangle
-  
-  attr_accessor :quilateral, :isosceles, :scalene
-  
-  def initialize(quilateral, isosceles, scalene)
-    @quilateral = quilateral
-    @isosceles = isosceles
-    @scalene = scalene
+  attr_accessor :side_1, :side_2, :side_3
+
+  def initialize(side_1, side_2, side_3)
+      @side_1 = side_1
+      @side_2 = side_2
+      @side_3 = side_3
   end
-  
-  def kind(symbol,type,valid_types)
+
+  def kind
+
+    if ((side_1 + side_2) < side_3) || ((side_1 + side_3) < side_2) || ((side_2 + side_3) < side_1) || !(side_1 > 0) || !(side_2 > 0) || !(side_3 > 0)
+        begin
+          raise TriangleError
+        rescue TriangleError
+        end
+
+    elsif @side_1 == @side_2 && @side_2 == @side_3 && @side_1 == @side_3
+      :equilateral
+
+    elsif (@side_1 == @side_2 && (@side_1 + @side_2) > @side_3) || (@side_1 == @side_3 && (@side_1 + @side_3) > @side_2) || (@side_2 == @side_3 && (@side_2 + @side_3) > @side_1)
+      :isosceles
+
+    elsif (@side_1 != @side_2) && (@side_1 != @side_3) && (@side_2 != @side_3)
+      :scalene
+
+    else
+      begin
+        raise TriangleError
+      rescue TriangleError
+      end
+    end
   end
-  
+
+  class TriangleError < StandardError
+  end
+
 end
